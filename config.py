dee84a01976bbca433f0a35519b139ae3145abdd
shashanktu <133744620+shashanktu@users.noreply.github.com>
2026-03-30 17:31:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TENANT_ID       = "5f3ec70f-0215-4f44-bdab-f5beda7cdd74"
+SUBSCRIPTION_ID = "46269e09-e1e1-4067-af90-78fd27b1b64d"
+RESOURCE_GROUP  = "devops-agent-rg"
+LOCATION        = "eastus"
+APP_NAME        = "my-app"
+DEPLOY_TARGET   = "app_service"
+BRANCH          = "main"
+ENABLE_SAST     = True
+ENABLE_DAST     = False
+APP_SERVICE_SKU = "B1"
